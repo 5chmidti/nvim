2303be90a0cb65645ecba86875a70fcd08b25dc5
5chmidti <44101708+5chmidti@users.noreply.github.com>
2024-04-20 20:46:01 +0200

    v3

--- init.lua
+++ init.lua
@@ -9,8 +9,6 @@ vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
 
 -- Make line numbers default
 vim.opt.number = true
@@ -561,46 +559,46 @@ require('lazy').setup({
    end,
  },
 
---  -- Highlight todo, notes, etc in comments
---  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
---
---  { -- Collection of various small independent plugins/modules
---    'echasnovski/mini.nvim',
---    config = function()
---      -- Better Around/Inside textobjects
---      --
---      -- Examples:
---      --  - va)  - [V]isually select [A]round [)]paren
---      --  - yinq - [Y]ank [I]nside [N]ext [']quote
---      --  - ci'  - [C]hange [I]nside [']quote
---      require('mini.ai').setup { n_lines = 500 }
---
---      -- Add/delete/replace surroundings (brackets, quotes, etc.)
---      --
---      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
---      -- - sd'   - [S]urround [D]elete [']quotes
---      -- - sr)'  - [S]urround [R]eplace [)] [']
---      require('mini.surround').setup()
---
---      -- Simple and easy statusline.
---      --  You could remove this setup call if you don't like it,
---      --  and try some other statusline plugin
---      local statusline = require 'mini.statusline'
---      -- set use_icons to true if you have a Nerd Font
---      statusline.setup { use_icons = vim.g.have_nerd_font }
---
---      -- You can configure sections in the statusline by overriding their
---      -- default behavior. For example, here we set the section for
---      -- cursor location to LINE:COLUMN
---      ---@diagnostic disable-next-line: duplicate-set-field
---      statusline.section_location = function()
---        return '%2l:%-2v'
---      end
---
---      -- ... and there is more!
---      --  Check out: https://github.com/echasnovski/mini.nvim
---    end,
---  },
+ -- Highlight todo, notes, etc in comments
+ { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+
+ { -- Collection of various small independent plugins/modules
+   'echasnovski/mini.nvim',
+   config = function()
+     -- Better Around/Inside textobjects
+     --
+     -- Examples:
+     --  - va)  - [V]isually select [A]round [)]paren
+     --  - yinq - [Y]ank [I]nside [N]ext [']quote
+     --  - ci'  - [C]hange [I]nside [']quote
+     require('mini.ai').setup { n_lines = 500 }
+
+     -- Add/delete/replace surroundings (brackets, quotes, etc.)
+     --
+     -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+     -- - sd'   - [S]urround [D]elete [']quotes
+     -- - sr)'  - [S]urround [R]eplace [)] [']
+     require('mini.surround').setup()
+
+     -- Simple and easy statusline.
+     --  You could remove this setup call if you don't like it,
+     --  and try some other statusline plugin
+     local statusline = require 'mini.statusline'
+     -- set use_icons to true if you have a Nerd Font
+     statusline.setup { use_icons = vim.g.have_nerd_font }
+
+     -- You can configure sections in the statusline by overriding their
+     -- default behavior. For example, here we set the section for
+     -- cursor location to LINE:COLUMN
+     ---@diagnostic disable-next-line: duplicate-set-field
+     statusline.section_location = function()
+       return '%2l:%-2v'
+     end
+
+     -- ... and there is more!
+     --  Check out: https://github.com/echasnovski/mini.nvim
+   end,
+ },
 --  { -- Highlight, edit, and navigate code
 --    'nvim-treesitter/nvim-treesitter',
 --    build = ':TSUpdate',
@@ -639,9 +637,6 @@ require('lazy').setup({
 --  -- place them in the correct locations.
 --
 --  -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
---  --
---  --  Here are some example plugins that I've included in the Kickstart repository.
---  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
 --  --
 --  -- require 'kickstart.plugins.debug',
 --  -- require 'kickstart.plugins.indent_line',
