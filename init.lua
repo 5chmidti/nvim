c53ed8c481590421e30647437941b0a14d9088fd
5chmidti <44101708+5chmidti@users.noreply.github.com>
2024-04-21 22:48:58 +0200

    rm commented out mason stuff

--- init.lua
+++ init.lua
@@ -266,11 +266,6 @@ require('lazy').setup({
   { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
     dependencies = {
-      -- Automatically install LSPs and related tools to stdpath for Neovim
-      -- 'williamboman/mason.nvim',
-      -- 'williamboman/mason-lspconfig.nvim',
-      -- 'WhoIsSethDaniel/mason-tool-installer.nvim',
-
       -- Useful status updates for LSP.
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       { 'j-hui/fidget.nvim', opts = {} },
