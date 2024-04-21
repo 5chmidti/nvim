e6590aa0356b03626d6e05b815c819ea75390efa
5chmidti <44101708+5chmidti@users.noreply.github.com>
2024-04-21 19:54:47 +0200

    support code actions in visual mode

--- init.lua
+++ init.lua
@@ -296,6 +296,10 @@ require('lazy').setup({
             vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
           end
 
+          local mapv = function(keys, func, desc)
+            vim.keymap.set('v', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+          end
+
           -- Jump to the definition of the word under your cursor.
           --  This is where a variable was first declared, or where a function is defined, etc.
           --  To jump back, press <C-t>.
@@ -327,7 +331,11 @@ require('lazy').setup({
 
           -- Execute a code action, usually your cursor needs to be on top of an error
           -- or a suggestion from your LSP for this to activate.
-          map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+          local code_action = function()
+            vim.lsp.buf.code_action({ apply = true })
+          end
+          map('<leader>ca', code_action, '[C]ode [A]ction')
+          mapv('<leader>ca', code_action, '[C]ode [A]ction')
 
           -- Opens a popup that displays documentation about the word under your cursor
           --  See `:help K` for why this keymap.
