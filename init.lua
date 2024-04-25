3f317b44a07b833725c8e688a4ac90b5cf4e6615
5chmidti <44101708+5chmidti@users.noreply.github.com>
2024-04-25 14:11:35 +0200

    move telescope and lsp configs into own files

--- init.lua
+++ init.lua
@@ -104,15 +104,26 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
-require('plugins')
---
----- The line beneath this is called `modeline`. See `:help modeline`
----- vim: ts=2 sts=2 sw=2 et
-----
+-- [[ Install `lazy.nvim` plugin manager ]]
+--  See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+    vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({ import = "plugins" }, {
+    ui = {
+        --  -- If you are using a Nerd Font: set icons to an empty table which will use the
+        --  -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+        icons = vim.g.have_nerd_font and {} or {},
+    },
+})
