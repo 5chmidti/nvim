8d3bdea9de4521153830a6facbbcdb6bcc1737bb
5chmidti <44101708+5chmidti@users.noreply.github.com>
2024-04-27 23:28:36 +0200

    add TelescopeUndo

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -68,6 +68,7 @@ return {
             vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
             vim.keymap.set('n', '<leader>sR', builtin.registers, { desc = '[S]earch [R]esume' })
             vim.keymap.set('n', '<leader>su', "<cmd>Telescope undo<cr>", { desc = '[S]earch [U]ndo' })
+            vim.keymap.set('n', '<leader>st', "<cmd>TodoTelescope<cr>", { desc = '[S]earch [T]odo' })
             vim.keymap.set('n', '<leader>s.', builtin.oldfiles,
                 { desc = '[S]earch Recent Files ("." for repeat)' })
             vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
