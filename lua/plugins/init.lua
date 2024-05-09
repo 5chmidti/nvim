99d10708c3fbe77a2d964dcb21e2e5a53eb15161
5chmidti <44101708+5chmidti@users.noreply.github.com>
2024-05-09 19:14:59 +0200

    add harpoon

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -51,11 +51,32 @@ return {
                         view = "notify",
                         filter = { event = "msg_showmode" }
                     }
-                }
+                },
             })
             vim.keymap.set('n', '<leader>nh', '<cmd>Noice history<cr>', { desc = '[N]oice [H]istory' })
             vim.keymap.set('n', '<leader>nl', '<cmd>Noice last<cr>', { desc = '[N]oice [L]ast' })
         end
+    },
+
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local harpoon = require('harpoon')
+            harpoon.setup()
+
+            vim.keymap.set('n', 'ho', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, {})
+            vim.keymap.set('n', 'ha', function() harpoon:list():add() end, {})
+
+            vim.keymap.set('n', 'h1', function() harpoon:list():select(1) end, {})
+            vim.keymap.set('n', 'h2', function() harpoon:list():select(2) end, {})
+            vim.keymap.set('n', 'h3', function() harpoon:list():select(3) end, {})
+            vim.keymap.set('n', 'h4', function() harpoon:list():select(4) end, {})
+
+            vim.keymap.set('n', 'hp', function() harpoon:list():prev() end, {})
+            vim.keymap.set('n', 'hn', function() harpoon:list():next() end, {})
+        end,
     },
 
     { -- You can easily change to a different colorscheme.
