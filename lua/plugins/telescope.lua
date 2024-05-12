945d98733e9a34be7a08cbab105fb4094febb3f7
5chmidti <44101708+5chmidti@users.noreply.github.com>
2024-05-12 22:34:13 +0200

    cleanup telescope keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -62,7 +62,6 @@ return {
             vim.keymap.set('n', '<leader>sF', builtin.find_files, { desc = '[S]earch [F]iles' })
             vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
             vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-            vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
             vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
             vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
             vim.keymap.set('n', '<leader>sR', builtin.registers, { desc = '[S]earch [R]esume' })
@@ -107,9 +106,9 @@ return {
 
             vim.keymap.set('n', '<leader>s?', function()
                 builtin.live_grep({
-                    prompt_title = 'Live Grep in all Files',
+                    prompt_title = 'Live Grep in All Files',
                 })
-            end, { desc = '[S]earch [?] in Open Files' })
+            end, { desc = '[S]earch [?] in All Files' })
 
             -- Shortcut for searching your Neovim configuration files
             vim.keymap.set('n', '<leader>sn', function()
