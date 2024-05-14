968d1744b0baa34ee743dae49103b1443d6c963a
5chmidti <44101708+5chmidti@users.noreply.github.com>
2024-05-14 18:28:22 +0200

    add split preview for substitution

--- init.lua
+++ init.lua
@@ -59,7 +59,7 @@ vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
-vim.opt.inccommand = 'nosplit'
+vim.opt.inccommand = 'split'
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
