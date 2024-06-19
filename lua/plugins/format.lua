fd9ba12a23fb6bac0bcb8bf229b63dcc8ea4693a
5chmidti <44101708+5chmidti@users.noreply.github.com>
2024-06-19 13:33:36 +0200

    update conforms lsp_fallback

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -50,7 +50,7 @@ return {
                 notify_on_error = false,
                 format_on_save = {
                     timeout_ms = 500,
-                    lsp_fallback = true,
+                    lsp_fallback = 'fallback',
                 },
             })
 
