6bf4ee28f7d645ec74d4e6a66a694dfc9303b788
5chmidti <44101708+5chmidti@users.noreply.github.com>
2024-08-21 13:17:16 +0200

    let noice override hover for stylization

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -44,9 +44,17 @@ return {
             --   `nvim-notify` is only needed, if you want to use the notification view.
             --   If not available, we use `mini` as the fallback
             "rcarriga/nvim-notify",
+            "hsh7th/nvim-cmp",
         },
         config = function()
             require('noice').setup({
+                lsp = {
+                    override = {
+                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                        ["vim.lsp.util.stylize_markdown"] = true,
+                        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+                    },
+                },
                 routes = {
                     {
                         view = "notify",
