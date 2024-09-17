a71b97c737e454d63f6e3bbfb51bfeb9b7cb204f
5chmidti <44101708+5chmidti@users.noreply.github.com>
2024-09-18 00:06:32 +0200

    feat(trouble): add initial trouble config

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,49 @@
+return {
+    {
+        "folke/trouble.nvim",
+        opts = {
+            modes = {
+                lsp_base = {
+                    params = {
+                        include_current = true, },
+                    win = {
+                        position = "right",
+                    },
+                },
+                lsp_document_symbols = {
+                    win = {
+                        position = "right",
+                    },
+                },
+            },
+        },
+        cmd = "Trouble",
+        keys = {
+            {
+                "<leader>vd",
+                "<cmd>Trouble diagnostics toggle<cr>",
+                desc = "Diagnostics (Trouble)",
+            },
+            {
+                "<leader>vf",
+                "<cmd>Trouble lsp toggle<cr>",
+                desc = "LSP (Trouble)",
+            },
+            {
+                "<leader>vr",
+                "<cmd>Trouble lsp_references toggle<cr>",
+                desc = "LSP references (Trouble)",
+            },
+            {
+                "<leader>vs",
+                "<cmd>Trouble symbols toggle<cr>",
+                desc = "Symbols (Trouble)",
+            },
+            {
+                "<leader>vt",
+                "<cmd>Trouble todo toggle<cr>",
+                desc = "ToDo (Trouble)",
+            },
+        },
+    },
+}
