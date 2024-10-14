3f92c57d20ad1c027d221eca05522701ab4036f0
5chmidti <44101708+5chmidti@users.noreply.github.com>
2024-10-14 13:09:22 +0200

    lint: add cmake-lint

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -3,11 +3,13 @@ return {
         'mfussenegger/nvim-lint',
         config = function()
             local lint = require('lint')
+            lint.linters.cmakelint.cmd = "cmake-lint"
             lint.linters_by_ft = {
                 fish = { "fish", },
                 lua = { "selene", },
                 sh = { "shellcheck", },
                 ['yaml.gha'] = { "actionlint", },
+                cmake = { "cmakelint", },
             }
             vim.api.nvim_create_autocmd({
                 'TextChanged',
